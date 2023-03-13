ca140ff66a12627644ae331ad297ef584e45b3df
dmitry <dima7mirnoff@gmail.com>
2023-03-13 16:16:47 +0300

    check deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/app'
 
+console.log('test')
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
