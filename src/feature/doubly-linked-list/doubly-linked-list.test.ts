2a3a7e7618d26f6eb147f1c7e25bc8f48bdbf1d3
dmitry <dima7mirnoff@gmail.com>
2023-03-17 15:53:59 +0300

    Add some changes

--- src/feature/doubly-linked-list/doubly-linked-list.test.ts
+++ src/feature/doubly-linked-list/doubly-linked-list.test.ts
@@ -2,7 +2,12 @@ import { describe, expect, test } from 'vitest'
 import { DoublyLinkedList } from './doubly-linked-list'
 
 describe('DoublyLinkedList', () => {
-  test('createInstance', () => {
+  test('creates an instance', () => {
+    const linkedList = new DoublyLinkedList<string>()
+    expect(linkedList).toBeTruthy()
+  })
+
+  test('append item', () => {
     const linkedList = new DoublyLinkedList<string>()
     linkedList.append('testValue')
 
