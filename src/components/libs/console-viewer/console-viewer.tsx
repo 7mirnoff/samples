c872224714b44431ceefa2ee68b62e61c2a4ad6e
dmitry <dima7mirnoff@gmail.com>
2023-03-30 12:32:29 +0300

    Add scaffolding styles

--- src/components/libs/console-viewer/console-viewer.tsx
+++ src/components/libs/console-viewer/console-viewer.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import classes from './console-viewer.module.scss'
 
 export const ConsoleViewer: React.FC = () => {
-  return <div className={classes.root} />
+  return <div className={classes.root}>text</div>
 }
