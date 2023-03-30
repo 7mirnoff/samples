6f6fdaab2717259a2d25e5ee80cf59cfd031da55
dmitry <dima7mirnoff@gmail.com>
2023-03-30 12:08:39 +0300

    Add linter for styles

---
+++ .stylelintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-rational-order",
+  ],
+  plugins: [
+    "stylelint-order",
+    "stylelint-scss"
+  ],
+  rules: {
+    // Дополнительные правила
+  },
+  customSyntax: "postcss-scss"
+}
