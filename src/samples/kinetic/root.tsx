a730c409126bb55fccd505e206b61e4dedc6efa0
dmitry <dima7mirnoff@gmail.com>
2023-04-02 22:07:56 +0300

    Add helper text in kinetic

--- src/samples/kinetic/root.tsx
+++ src/samples/kinetic/root.tsx
@@ -6,6 +6,7 @@ export const KineticRoot: React.FC = () => {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Scene />
+      <h2 style={{ position: 'absolute', top: 0, left: 0, textAlign: 'center', width: '100%' }}>Управление WASD</h2>
     </div>
   )
 }
